6e6b183d1fdb7f431aa75915389b694ea1389b05
Juancro27 <juankopy@gmail.com>
2020-10-30 02:30:04 -0300

    Finished homework v

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,6 +9,19 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+  function Usuario (opciones) {
+    
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+
+  }
+Usuario.prototype.saludar = function() {return 'Hola, mi nombre es ' + this.nombre}
+
+return Usuario;
+
+
 }
 
 function agregarMetodoPrototype(Constructor) {
@@ -16,6 +29,8 @@ function agregarMetodoPrototype(Constructor) {
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
 
+  Constructor.prototype.saludar = function () {return 'Hello World!'}
+  return agregarMetodoPrototype;
 }
 
 function agregarStringInvertida() {
@@ -23,8 +38,17 @@ function agregarStringInvertida() {
   // El método debe llamarse "reverse"
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
-  // Pista: Necesitarás usar "this" dentro de "reverse"
+  // Pista: Necesitarás usar "this" dentro de "reverse" 
 
+  String.prototype.reverse = function () { var mirror = '';
+
+    for (i = 0; i < this.length; i++) {
+
+      mirror = this[i] + mirror;
+
+    }
+      return mirror;
+  }
 }
 
 // No modificar nada debajo de esta línea
